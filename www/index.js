2401c8456ef3754471d4511eeca52ec45d20289e
Austin Clements <austinclementsbass@gmail.com>
2020-10-06 12:46:56 -0500

    Add flex box css, turn most UI back on web page

--- www/index.js
+++ www/index.js
@@ -21,76 +21,76 @@ let simTicksPerFrame = 1;
 // let canvas = document.getElementById("game-canvas");
 // const ctx = canvas.getContext("2d");
 
-// let gravityWellMassSlider = document.getElementById("gravity-well-mass");
-// gravityWellMassSlider.addEventListener("change", (e) => {
-//   rustCanvas.set_gravity_well_mass(gravityWellMassSlider.value);
-// });
+let gravityWellMassSlider = document.getElementById("gravity-well-mass");
+gravityWellMassSlider.addEventListener("change", (e) => {
+  rustCanvas.set_gravity_well_mass(gravityWellMassSlider.value);
+});
 
 let particleCountElement = document.getElementById("particle-count");
 
-// let clearParticlesButton = document.getElementById("clear-particles-button");
-// clearParticlesButton.addEventListener("click", (e) => {
-//   rustCanvas.clear_particles();
-// });
+let clearParticlesButton = document.getElementById("clear-particles-button");
+clearParticlesButton.addEventListener("click", (e) => {
+  rustCanvas.clear_particles();
+});
 
-// let removeSomeParticlesButton = document.getElementById(
-//   "remove-some-particles-button"
-// );
-// removeSomeParticlesButton.addEventListener("click", (e) => {
-//   rustCanvas.remove_particles(250);
-// });
+let removeSomeParticlesButton = document.getElementById(
+  "remove-some-particles-button"
+);
+removeSomeParticlesButton.addEventListener("click", (e) => {
+  rustCanvas.remove_particles(250);
+});
 
-// let bordersActiveCheckbox = document.getElementById("borders-active-checkbox");
-// bordersActiveCheckbox.addEventListener("input", (e) => {
-//   rustCanvas.set_borders_active(bordersActiveCheckbox.checked);
-// });
+let bordersActiveCheckbox = document.getElementById("borders-active-checkbox");
+bordersActiveCheckbox.addEventListener("input", (e) => {
+  rustCanvas.set_borders_active(bordersActiveCheckbox.checked);
+});
 
-// let screenClearCheckbox = document.getElementById("clear-screen-checkbox");
-// screenClearCheckbox.addEventListener("input", (e) => {
-//   rustCanvas.set_should_clear_screen(screenClearCheckbox.checked);
-// });
+let screenClearCheckbox = document.getElementById("clear-screen-checkbox");
+screenClearCheckbox.addEventListener("input", (e) => {
+  rustCanvas.set_should_clear_screen(screenClearCheckbox.checked);
+});
 
-// let simSpeedDownButton = document.getElementById("sim-speed-down");
-// let simSpeedUpButton = document.getElementById("sim-speed-up");
-// simSpeedDownButton.addEventListener("click", (e) => {
-//   if (simTicksPerFrame > 1) {
-//     simTicksPerFrame -= 1;
-//   }
-//   updateSimSpeedLabel();
-// });
-// simSpeedUpButton.addEventListener("click", (e) => {
-//   simTicksPerFrame += 1;
-//   updateSimSpeedLabel();
-// });
-// const updateSimSpeedLabel = () => {
-//   document.getElementById(
-//     "sim-speed-label"
-//   ).textContent = `Simulation Speed: x${simTicksPerFrame}`;
-// };
+let simSpeedDownButton = document.getElementById("sim-speed-down");
+let simSpeedUpButton = document.getElementById("sim-speed-up");
+simSpeedDownButton.addEventListener("click", (e) => {
+  if (simTicksPerFrame > 1) {
+    simTicksPerFrame -= 1;
+  }
+  updateSimSpeedLabel();
+});
+simSpeedUpButton.addEventListener("click", (e) => {
+  simTicksPerFrame += 1;
+  updateSimSpeedLabel();
+});
+const updateSimSpeedLabel = () => {
+  document.getElementById(
+    "sim-speed-label"
+  ).textContent = `Simulation Speed: x${simTicksPerFrame}`;
+};
 
-// let particleTrailLengthDownButton = document.getElementById(
-//   "particle-trail-length-down"
-// );
-// let particleTrailLengthUpButton = document.getElementById(
-//   "particle-trail-length-up"
-// );
-// particleTrailLengthDownButton.addEventListener("click", (e) => {
-//   if (particleTrailLength > 1) {
-//     particleTrailLength -= 1;
-//   }
-//   rustCanvas.set_particle_trail_length(particleTrailLength);
-//   updateParticleTrailLengthLabel();
-// });
-// particleTrailLengthUpButton.addEventListener("click", (e) => {
-//   particleTrailLength += 1;
-//   rustCanvas.set_particle_trail_length(particleTrailLength);
-//   updateParticleTrailLengthLabel();
-// });
-// const updateParticleTrailLengthLabel = () => {
-//   document.getElementById(
-//     "particle-trail-length-label"
-//   ).textContent = `Particle Trail Length: ${particleTrailLength}`;
-// };
+let particleTrailLengthDownButton = document.getElementById(
+  "particle-trail-length-down"
+);
+let particleTrailLengthUpButton = document.getElementById(
+  "particle-trail-length-up"
+);
+particleTrailLengthDownButton.addEventListener("click", (e) => {
+  if (particleTrailLength > 1) {
+    particleTrailLength -= 1;
+  }
+  rustCanvas.set_particle_trail_length(particleTrailLength);
+  updateParticleTrailLengthLabel();
+});
+particleTrailLengthUpButton.addEventListener("click", (e) => {
+  particleTrailLength += 1;
+  rustCanvas.set_particle_trail_length(particleTrailLength);
+  updateParticleTrailLengthLabel();
+});
+const updateParticleTrailLengthLabel = () => {
+  document.getElementById(
+    "particle-trail-length-label"
+  ).textContent = `Particle Trail Length: ${particleTrailLength}`;
+};
 
 window.oncontextmenu = (e) => {
   e.preventDefault();
@@ -130,22 +130,9 @@ window.addEventListener("pointerup", (e) => {
   }
 });
 
-// const rustCanvas = RustCanvas.new(CANVAS_WIDTH, CANVAS_HEIGHT);
 const rustCanvas = RustCanvas.new();
-// rustCanvas.spawn_gravity_well(canvas.width / 2.0, canvas.height / 2.0);
+rustCanvas.spawn_gravity_well(canvas.width / 2.0, canvas.height / 2.0);
 rustCanvas.initialize_particles(10000);
-
-// let renderToCanvas = () => {
-//   let pixelDataPtr = rustCanvas.get_pixel_buffer_ptr();
-//   let pixelData = new Uint8ClampedArray(
-//     memory.buffer,
-//     pixelDataPtr,
-//     // CANVAS_WIDTH * CANVAS_HEIGHT * 4
-//     canvas.width * canvas.height * 4
-//   );
-//   let image = new ImageData(pixelData, canvas.width, canvas.height);
-//   ctx.putImageData(image, 0, 0);
-// };
 
 const frameRateCounter = new (class {
   constructor() {
@@ -208,8 +195,7 @@ const renderLoop = () => {
   timeSinceUpdate += frameDelta;
   timeSinceRender += frameDelta;
   updateParticleCountLabel();
-  // updateSimSpeedLabel();
-  // updateParticleTrailLengthLabel();
+  updateSimSpeedLabel();
   let updatesThisFrame = 0;
   do {
     for (let i = 0; i < simTicksPerFrame; i++) {
@@ -227,7 +213,6 @@ const renderLoop = () => {
     updatesThisFrame <= MAX_UPDATES_PER_FRAME
   );
   rustCanvas.render();
-  // renderToCanvas();
   requestAnimationFrame(renderLoop);
 };
 requestAnimationFrame(renderLoop);
